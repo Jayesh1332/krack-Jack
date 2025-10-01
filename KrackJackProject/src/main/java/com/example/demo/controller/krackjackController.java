1a8ee9edd13cd13263d901c29e6ca6e7a8a97331
Jayesh1332 <jayeshthinkpad@gmail.com>
2025-10-01 19:52:39 +0530

    edited again

--- KrackJackProject/src/main/java/com/example/demo/controller/krackjackController.java
+++ KrackJackProject/src/main/java/com/example/demo/controller/krackjackController.java
@@ -1,8 +1,19 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class krackjackController {
 
 	public static void main(String[] args) {
 	
+		
+	 System.out.println("Hello this is jayesh");
+	
+	}
+	
+	public static void m1 () {
+		System.out.println("tried to add some methods in it");
 	}
 }
